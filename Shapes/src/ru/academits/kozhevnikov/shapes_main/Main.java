a9fba19029534f30a1c4ac46043f7cf57a78a288
Vadim4041 <kozhevnikov404@yandex.ru>
2023-02-08 15:34:49 +0700

    updated Shapes + part 2

--- Shapes/src/ru/academits/kozhevnikov/shapes_main/Main.java
+++ Shapes/src/ru/academits/kozhevnikov/shapes_main/Main.java
@@ -2,31 +2,38 @@ package ru.academits.kozhevnikov.shapes_main;
 
 import ru.academits.kozhevnikov.shapes.*;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Shape square = new Square(5);
-        Shape rectangle = new Rectangle(5, 2);
-        Shape circle = new Circle(5);
-        Shape triangle = new Triangle(0, 0, 5, 0, 0, 6);
+        Shape[] shapes = new Shape[]{
+                new Square(5),
+                new Square(4.99),
+                new Rectangle(5, 2),
+                new Circle(5),
+                new Triangle(0, 0, 5, 0, 0, 6),
+                new Square(8),
+                new Rectangle(7, 3),
+                new Circle(9),
+                new Triangle(0, 0, 9, 0, 0, 2),
+        };
 
-        System.out.println(square.getArea());
-        System.out.println(square.getPerimeter());
-        System.out.println(square.getWidth());
-        System.out.println(square.getHeight());
+        Arrays.sort(shapes, new SortByArea());
+
+        System.out.println("Характеристики фигуры с максимальной площадью:");
+        System.out.println("Ширина: " + shapes[0].getWidth());
+        System.out.println("Высота: " + shapes[0].getHeight());
+        System.out.println("Площадь: " + shapes[0].getArea());
+        System.out.println("Периметр: " + shapes[0].getPerimeter());
+
         System.out.println();
-        System.out.println(rectangle.getArea());
-        System.out.println(rectangle.getPerimeter());
-        System.out.println(rectangle.getWidth());
-        System.out.println(rectangle.getHeight());
-        System.out.println();
-        System.out.println(circle.getArea());
-        System.out.println(circle.getPerimeter());
-        System.out.println(circle.getWidth());
-        System.out.println(circle.getHeight());
-        System.out.println();
-        System.out.println(triangle.getArea());
-        System.out.println(triangle.getPerimeter());
-        System.out.println(triangle.getWidth());
-        System.out.println(triangle.getHeight());
+
+        Arrays.sort(shapes, new SortByPerimeter());
+
+        System.out.println("Характеристики фигуры со вторым по величине периметром:");
+        System.out.println("Ширина: " + shapes[1].getWidth());
+        System.out.println("Высота: " + shapes[1].getHeight());
+        System.out.println("Площадь: " + shapes[1].getArea());
+        System.out.println("Периметр: " + shapes[1].getPerimeter());
     }
 }
