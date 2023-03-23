64b9a2bf55330791711680f4b79f49f8899ad0c3
Vadim4041 <kozhevnikov404@yandex.ru>
2023-03-23 10:21:58 +0700

    added first sketch for Tree

---
+++ Tree/src/ru/academits/kozhevikov/tree/Tree.java
@@ -0,0 +1,13 @@
+package ru.academits.kozhevikov.tree;
+
+public class Tree<T> implements Comparable<T> {
+    private int size;
+    private TreeNode<T> root;
+
+    @Override
+    public int compareTo(T o) {
+        return 0;
+    }
+
+
+}
---
+++ Tree/src/ru/academits/kozhevikov/tree_main/Main.java
@@ -0,0 +1,11 @@
+package ru.academits.kozhevikov.tree_main;
+
+import ru.academits.kozhevikov.tree.Tree;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree<Integer> tree = new Tree<>();
+
+        System.out.println(tree);
+    }
+}
