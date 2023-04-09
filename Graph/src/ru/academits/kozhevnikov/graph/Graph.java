4db612645117204c8ece8694035f347d5ac1d26b
Vadim4041 <kozhevnikov404@yandex.ru>
2023-04-09 15:14:40 +0700

    updated Graph

--- Graph/src/ru/academits/kozhevnikov/graph/Graph.java
+++ Graph/src/ru/academits/kozhevnikov/graph/Graph.java
@@ -5,28 +5,34 @@ import java.util.Queue;
 import java.util.function.IntConsumer;
 
 public class Graph {
-    private final int[][] adjacentMatrix;
+    private final int[][] adjacencyMatrix;
     private final int verticesCount;
 
     public Graph(int verticesCount) {
         if (verticesCount <= 0) {
-            throw new IllegalArgumentException("Number of vertices must be positive.");
+            throw new IllegalArgumentException(String.format("Number of vertices %d: Number of vertices must grater than zero.", verticesCount));
         }
 
         this.verticesCount = verticesCount;
-        adjacentMatrix = new int[verticesCount][verticesCount];
+        adjacencyMatrix = new int[verticesCount][verticesCount];
     }
 
     public void addEdge(int startVertexIndex, int endVertexIndex) {
-        if (startVertexIndex < 0 || startVertexIndex >= verticesCount || endVertexIndex < 0 || endVertexIndex >= verticesCount) {
-            throw new IllegalArgumentException("Invalid vertex index.");
+        if (startVertexIndex < 0 || startVertexIndex >= verticesCount) {
+            throw new IndexOutOfBoundsException(String.format("Start vertex index: %d. " +
+                    "Start vertex index must be positive and less then the total number of vertices: %d", startVertexIndex, verticesCount));
         }
 
-        adjacentMatrix[startVertexIndex][endVertexIndex] = 1;
-        adjacentMatrix[endVertexIndex][startVertexIndex] = 1;
+        if (endVertexIndex < 0 || endVertexIndex >= verticesCount) {
+            throw new IndexOutOfBoundsException(String.format("End vertex index: %d. " +
+                    "End vertex index must be positive and less then the total number of vertices: %d", endVertexIndex, verticesCount));
+        }
+
+        adjacencyMatrix[startVertexIndex][endVertexIndex] = 1;
+        adjacencyMatrix[endVertexIndex][startVertexIndex] = 1;
     }
 
-    public void breadthFirstTraversal(IntConsumer consumer) {
+    public void traverseBreadthFirst(IntConsumer consumer) {
         boolean[] visited = new boolean[verticesCount];
         Queue<Integer> queue = new LinkedList<>();
 
@@ -40,7 +46,7 @@ public class Graph {
                     consumer.accept(vertex);
 
                     for (int j = 0; j < verticesCount; j++) {
-                        if (adjacentMatrix[vertex][j] == 1 && !visited[j]) {
+                        if (adjacencyMatrix[vertex][j] == 1 && !visited[j]) {
                             visited[j] = true;
                             queue.add(j);
                         }
@@ -50,22 +56,54 @@ public class Graph {
         }
     }
 
-    public void depthFirstTraversal(IntConsumer consumer) {
+    public void traverseDepthFirstRecursively(IntConsumer consumer) {
         boolean[] visited = new boolean[verticesCount];
+
         for (int i = 0; i < verticesCount; i++) {
             if (!visited[i]) {
-                depthFirstRecursive(i, visited, consumer);
+                traverseDepthFirstRecursively(i, visited, consumer);
             }
         }
     }
 
-    private void depthFirstRecursive(int vertex, boolean[] visited, IntConsumer consumer) {
+    private void traverseDepthFirstRecursively(int vertex, boolean[] visited, IntConsumer consumer) {
         visited[vertex] = true;
         consumer.accept(vertex);
 
         for (int i = 0; i < verticesCount; i++) {
-            if (adjacentMatrix[vertex][i] == 1 && !visited[i]) {
-                depthFirstRecursive(i, visited, consumer);
+            if (adjacencyMatrix[vertex][i] == 1 && !visited[i]) {
+                traverseDepthFirstRecursively(i, visited, consumer);
+            }
+        }
+    }
+
+    public void traverseDepthFirst(IntConsumer consumer) {
+        boolean[] visited = new boolean[verticesCount];
+        for (int i = 0; i < verticesCount; i++) {
+            if (!visited[i]) {
+                traverseDepthFirst(i, visited, consumer);
+            }
+        }
+    }
+
+    private void traverseDepthFirst(int startVertex, boolean[] visited, IntConsumer consumer) {
+        visited[startVertex] = true;
+        consumer.accept(startVertex);
+        int[] stack = new int[verticesCount];
+        int top = 0;
+        stack[top] = startVertex;
+
+        while (top >= 0) {
+            int vertex = stack[top];
+            top--;
+
+            for (int i = verticesCount - 1; i >= 0; i--) {
+                if (adjacencyMatrix[vertex][i] == 1 && !visited[i]) {
+                    visited[i] = true;
+                    consumer.accept(i);
+                    top++;
+                    stack[top] = i;
+                }
             }
         }
     }
--- Graph/src/ru/academits/kozhevnikov/graph_main/Main.java
+++ Graph/src/ru/academits/kozhevnikov/graph_main/Main.java
@@ -2,21 +2,43 @@ package ru.academits.kozhevnikov.graph_main;
 
 import ru.academits.kozhevnikov.graph.Graph;
 
+import java.util.function.IntConsumer;
+
 public class Main {
     public static void main(String[] args) {
-        Graph graph = new Graph(9);
+        Graph graph = new Graph(8);
 
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
-        graph.addEdge(1, 2);
-        graph.addEdge(3, 4);
-        graph.addEdge(4, 5);
-        graph.addEdge(6, 6);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(2, 6);
+        graph.addEdge(3, 7);
 
-        System.out.println("Breadth-first traversal:");
-        graph.breadthFirstTraversal(v -> System.out.print(v + " "));
+        /*
+        Tree markdown:
+             0
+           /   \
+          1     2
+         / \   / \
+        3   4 5   6
+             |
+             7
+         */
+
+        IntConsumer consumer = v -> System.out.print(v + " ");
+
+        System.out.println("Breadth-first traversal:"); // 0 1 2 3 4 5 6 7
+        graph.traverseBreadthFirst(consumer);
         System.out.println();
-        System.out.println("Depth-first traversal:");
-        graph.depthFirstTraversal(v -> System.out.print(v + " "));
+
+        System.out.println("Depth-first traversal (non-recursive):"); // 0 2 6 5 1 4 3 7
+        graph.traverseDepthFirstRecursively(consumer);
+        System.out.println();
+
+        System.out.println("Depth-first traversal (recursive):"); // 0 1 3 7 4 2 5 6
+        graph.traverseDepthFirst(consumer);
+
     }
 }
